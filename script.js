8097af9145ffd5e25b07aac1486c1e7310a82cb6
Mike <mike.perez1189@gmail.com>
2025-06-03 13:35:09 -0500

    Update firebase config

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 // script.js for SpaceJamz EPK
 
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
 document.addEventListener('DOMContentLoaded', () => {
     // --- Firebase Initialization ---
     let db, auth;
